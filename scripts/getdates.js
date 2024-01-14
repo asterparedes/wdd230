3f730a8b3978a9f02f1e94f79f1ce247a6191362
asterparedes <asterparedes0912@gmail.com>
2024-01-13 22:45:58 -0800

    changed the js code

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 document.querySelector('#year').textContent = new Date().getFullYear();
 
 const lastModifiedDate = document.lastModified;
-document.querySelector('#lastModified').innerText = `Last Modification: ${lastModifiedDate}`;
+document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModifiedDate}`;
