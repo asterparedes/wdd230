d18cc6ee0737556058c5e29189dc04b099f546ab
asterparedes <asterparedes0912@gmail.com>
2024-01-25 22:11:59 -0800

    added responsive menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,11 @@ document.querySelector('#year').textContent = new Date().getFullYear();
 
 const lastModifiedDate = document.lastModified;
 document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModifiedDate}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
