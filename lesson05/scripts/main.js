b745afda52ee16e85be0aed5907bdbfbf1ac5479
asterparedes <asterparedes0912@gmail.com>
2024-02-15 21:44:33 -0800

    added responsive images in my course home page

--- lesson05/scripts/main.js
+++ lesson05/scripts/main.js
@@ -14,6 +14,7 @@ button.addEventListener('click', () => {
 
         deleteButton.addEventListener('click', () => {
             list.removeChild(listChapter);
+            input.focus();
         });
         
         input.focus();
@@ -32,6 +33,7 @@ button.addEventListener('click', () => {
 
     // deleteButton.addEventListener('click', () => {
     //     list.removeChild(listChapter);
+    //     input.focus();
     // });
     
     // input.focus();
