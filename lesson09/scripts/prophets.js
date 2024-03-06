3188565ee4cf99be6cbfa519386e811b3cca81fc
aster <asterparedes0912@gmail.com>
2024-03-06 00:05:33 -0800

    added fetch json file

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,39 @@
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+        // Create elements
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        // Build the h2 content
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        // Build the image portrait
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '250');
+        portrait.setAttribute('height', '300');
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+};
+
+const getProphetData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+}
+
+getProphetData();
