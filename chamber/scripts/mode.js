7e64798410c9d4b6a8aff4f35b2c89e97923d027
asterparedes <asterparedes0912@gmail.com>
2024-03-21 20:59:32 -0700

    chamber of commerce page

--- chamber/scripts/mode.js
+++ chamber/scripts/mode.js
@@ -2,13 +2,13 @@ const modeButton = document.querySelector('#mode');
 const main = document.querySelector('main');
 
 modeButton.addEventListener('click', () => {
-    if (modeButton.textContent.includes('🌑')) {
+    if (modeButton.textContent.includes('🌙')) {
         main.style.background = '#000';
         main.style.color = '#fff';
         modeButton.textContent = '🔆';
     } else {
         main.style.background = '#FAF3DD';
         main.style.color = '#32292F';
-        modeButton.textContent = '🌑';
+        modeButton.textContent = '🌙';
     }
 });
